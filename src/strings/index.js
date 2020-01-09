a161b86c2521a62e941a423497b737f947ed9c15
ajewe <ajewe@utexas.edu>
2020-01-08 18:33:19 -0600

    did string functions

--- src/strings/index.js
+++ src/strings/index.js
@@ -1,16 +1,25 @@
 const split = (str, delim) => {
   // write code for strings.split
-  return str.split()
+  return str.split(delim);
 }
 
 const pairs = (str) => {
   // write code for strings.pairs
+  let arrStr = str.split('');
+  let arrPair = [];
 
+  for (let i = 0; i < arrStr.length; i = i + 2) {
+    arrPair.push(arrStr[i] + arrStr[i + 1])
+  }
+  return arrPair;
 }
 
 const reverse = (str) => {
   // write code for strings.reverse
-
+  let arr = str.split('');
+  let reverseArr = arr.reverse();
+  let reverseStr = reverseArr.join('');
+  return reverseStr;
 }
 
 module.exports = {
